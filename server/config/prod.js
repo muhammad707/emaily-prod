8f628b613941db19e1241a430f1fb4dc1a50998c
MSM_707 <sirojiddinov.1881@mail.ru>
2018-09-21 10:58:22 +0500

    little tweak in config files

--- server/config/prod.js
+++ server/config/prod.js
@@ -7,5 +7,5 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretKey: STRIPE_SECRET_KEY
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
